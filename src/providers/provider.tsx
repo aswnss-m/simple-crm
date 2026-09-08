56fde5491d44e608c7a4a575b8a9de1732ed4aca
Aswin Lal <aswnss.m@gmail.com>
2026-09-08 17:11:05 +0530

    init

---
+++ src/providers/provider.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "./theme-provider"
+export default function Providers({ children }: { children: React.ReactNode }) {
+    return (
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        >
+            {children}
+        </ThemeProvider>
+    );
+}
