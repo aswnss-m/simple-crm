e9d70055f97a6ca075ede12d14ac3d8a466c50d6
Aswin Lal <aswnss.m@gmail.com>
2026-09-12 16:41:24 +0530

    auth check in the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
 import Link from "next/link";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  // already logged in
+  if (session) {
+    redirect("/overview");
+  }
   return (
-<div className="flex items-center justify-center h-screen w-full">
-  <div className="flex gap-4">
-  <Link href="/login" type="button" tabIndex={0}>Login</Link>
-  <Link href="/register" type="button " tabIndex={0}>Register</Link>
-
-  </div>
-</div>
+    <div className="flex items-center justify-center h-screen w-full">
+      <div className="flex gap-4">
+        <Link href="/login" type="button" tabIndex={0}>
+          Login
+        </Link>
+        <Link href="/register" type="button " tabIndex={0}>
+          Register
+        </Link>
+      </div>
+    </div>
   );
 }
